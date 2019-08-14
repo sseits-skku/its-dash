65d47ba8eeacb72b4e7fb62c5ae8c4a905c32f22
Vaelot <minusgodkeanu@gmail.com>
2019-08-14 10:34:25 +0900

    refactor unused files

---
+++ components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Member Login</v-toolbar-title>
+                <!--
+                <v-spacer />
+                -->
+              </v-toolbar>
+              <v-card-text>
+                <v-text-field
+                  :model="id"
+                  label="Login"
+                  name="login"
+                  prepend-icon="person"
+                  type="text"
+                />
+                <v-text-field
+                  :model="pw"
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"
+                />
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    id: '',
+    pw: ''
+  })
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,6 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
@@ -80,13 +76,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
